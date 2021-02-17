97d1d8a970357d4830ae8bc3499440a546a8a354
Thanos Lentzos <thanoslentzos@gmail.com>
2021-02-17 12:16:55 +0200

    created example class

---
+++ src/main/java/org/exercise/ExampleClass.java
@@ -0,0 +1,11 @@
+package org.exercise;
+
+public class ExampleClass {
+    public static void main(String[] args){
+        //single lin commetns
+        /*
+        multi line commoents
+         */
+        System.out.println("i did it");
+    }
+}
